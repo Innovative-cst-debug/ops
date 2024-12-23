4898d6f8ffd416da269edfe7e302e5b950170e8f
SyntaxGalaxy <96680248+SyntaxGalaxy@users.noreply.github.com>
2024-12-23 22:27:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+DMD2
+DS3231 RTC Library: real-time clock
+Adafruit GFX Library: Required by DMD2
+```
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <DMD2.h>
+#include <fonts/SystemFont5x7.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+int WIDTH 32
+int PANELS 1
+SoftDMD dmd(WIDTH * PANELS, 16, 1);
+
+RTC_DS3231 rtc;
+
+void setup() {
+  Serial.begin(9600);
+
+  dmd.setBrightness(255);
+  dmd.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, setting the time.");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop() {
+  dmd.clearScreen();
+
+  dmd.selectFont(SystemFont5x7);
+  dmd.drawMarquee("Oxford Dreen Public School", 27, 0, 0);
+
+  DateTime now = rtc.now();
+  char timeStr[9];
+  sprintf(timeStr, "%02d:%02d", now.hour(), now.second());
+  dmd.drawString(0, 9, timeStr, GRAPHICS_NORMAL);
+
+  dmd.refresh();
+
+  delay(1);
+}
